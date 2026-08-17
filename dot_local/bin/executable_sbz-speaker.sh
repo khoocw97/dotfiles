248660f344674daa10b32c3e2225b1bf03000733
khoocw97 <khoocw97@gmail.com>
2026-08-18 00:53:30 +0800

    fix sound blaster z

---
+++ dot_local/bin/executable_sbz-speaker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+CARD=Creative
+CARD_ID="alsa_card.pci-0000_04_00.0"
+
+pactl set-card-profile "$CARD_ID" output:analog-stereo+input:analog-stereo
+sleep 0.3
+pactl set-default-sink "alsa_output.pci-0000_04_00.0.analog-stereo"
+amixer -c "$CARD" sset 'PCM' 100%
+amixer -c "$CARD" sset Front 9dB unmute
+amixer -c "$CARD" -M sset Master 29%
+amixer -c "$CARD" sset 'Enable OutFX' unmute
+amixer -c "$CARD" sset 'HP/Speaker Auto Detect' off
+amixer -c "$CARD" sset 'Full-Range Front Speakers' unmute
+amixer -c "$CARD" sset 'Output Select' Speakers
+amixer -c "$CARD" sset 'FX: X-Bass' on
+amixer -c "$CARD" sset 'FX: X-Bass Crossover' 35
+
---
+++ dot_local/bin/executable_sbz-toggle-headphone.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+CARD=Creative
+CARD_ID="alsa_card.pci-0000_04_00.0"
+
+if [[ $(pactl get-default-sink) == *analog* ]]; then
+    # 音箱 -> 光纤耳机
+    pactl set-card-profile "$CARD_ID" output:iec958-stereo+input:analog-stereo
+    sleep 0.3
+    pactl set-default-sink "alsa_output.pci-0000_04_00.0.iec958-stereo"
+    amixer -c "$CARD" sset 'IEC958' on
+    amixer -c "$CARD" sset Front mute
+    amixer -c "$CARD" sset 'Enable OutFX' off
+    notify-send --app-name="音频模式" "已切至：光纤耳机 (纯净)" --icon=audio-headphones
+else
+    # 光纤/其他 -> 音箱 (复用 startup 脚本)
+    ~/.local/bin/sbz-speaker.sh
+    notify-send --app-name="音频模式" "已切至：Soundbar (35Hz 音效)" --icon=audio-speakers
+fi
+
