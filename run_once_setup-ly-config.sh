f8bad821e7e815d9f3410161c94fda0d74d69799
khoocw97 <khoocw97@gmail.com>
2026-09-08 03:41:20 +0800

    update ly

--- run_once_setup-ly-config.sh
+++ run_once_setup-ly-config.sh
@@ -2,10 +2,10 @@
 set -e
 TARGET_DIR="/etc/ly"
 TARGET_FILE="$TARGET_DIR/config.ini"
-SOURCE_FILE="$CHEZMOI_SOURCE_DIR/private_dot_config/ly/config.ini"
+SOURCE_FILE="$CHEZMOI_SOURCE_DIR/system_files/ly/config.ini"
 
 if [ ! -f "$SOURCE_FILE" ]; then
-    echo "❌ 错误: 找不到 $SOURCE_FILE"
+    echo "❌ no $SOURCE_FILE"
     exit 1
 fi
 
@@ -13,4 +13,4 @@ sudo mkdir -p "$TARGET_DIR"
 sudo cp "$SOURCE_FILE" "$TARGET_FILE"
 sudo chown root:root "$TARGET_FILE"
 sudo chmod 644 "$TARGET_FILE"
-echo "✅ /etc/ly/config.ini 已成功部署"
+echo "✅ /etc/ly/config.ini deploy"
